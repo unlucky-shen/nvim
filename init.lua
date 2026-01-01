021c5c7bebdcc66af5499c0c1f19a7f25717f79e
Taufiq <taufiqhaikal002@gmail.com>
2026-01-02 00:18:22 +0800

    Added Transparency

--- init.lua
+++ init.lua
@@ -53,6 +53,7 @@ vim.pack.add({
 vim.opt.background = 'dark'
 vim.g.gruvbox_material_background = 'hard'
 vim.g.gruvbox_material_foreground = 'material'
+vim.g.gruvbox_material_transparent_background = '2'
 vim.cmd('colorscheme gruvbox-material')
 
 -- Lualine
